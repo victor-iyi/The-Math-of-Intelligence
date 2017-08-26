68880441f2f094539590013dc160fb29bdcd06a9
victor-iyiola <javafolabi@gmail.com>
2017-08-26 22:21:25 +0100

    Support Vector machine

---
+++ support vector machine/svm.py
@@ -0,0 +1,7 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 26 August, 2017 @ 10:20 PM.
+  Copyright (c) 2017. victor. All rights reserved.
+"""
