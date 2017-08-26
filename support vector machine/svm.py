c51b8819f4a8b52ceea1681c2004e54d8edf396d
victor-iyiola <javafolabi@gmail.com>
2017-08-27 00:09:40 +0100

    Linear Regression predict method

--- support vector machine/svm.py
+++ support vector machine/svm.py
@@ -6,3 +6,33 @@
   Copyright (c) 2017. victor. All rights reserved.
 """
 
+import numpy as np
+
+
+class SVC(object):
+    def __init__(self):
+        self.w = 0
+
+
+def fit(self, X, y):
+    pass
+
+
+def predict(self):
+    pass
+
+
+if __name__ == '__main__':
+    # Input [x, y, bias]
+    X = np.array([
+        [-2, 4, -1],
+        [4, 1, -1],
+        [1, 6, -1],
+        [2, 4, -1],
+        [6, 2, -1]
+    ])
+    # Labels
+    y = np.array([-1, -1, 1, 1, 1])
+
+    svm = SVC()
+    svm.fit(X, y)
