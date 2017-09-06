3c09ee34fa2545ade994dc4f9e06e80f50a7e796
victor-iyiola <javafolabi@gmail.com>
2017-09-06 22:50:34 +0100

    Cluster centers refactored

--- flat clustering/cluster.py
+++ flat clustering/cluster.py
@@ -12,7 +12,7 @@ from scipy.spatial.distance import euclidean
 
 class KMeans(object):
     def __init__(self, n_clusters=8, max_iter=300, tol=1e-3):
-        self.cluster_centers_ = dict()
+        self.cluster_centers_ = None
         self.classes_ = None
         self.n_clusters = n_clusters
         self.max_iter = max_iter
@@ -20,10 +20,9 @@ class KMeans(object):
 
     def fit(self, X):
         # !- Select first k data points as centroids
-        for i in range(self.n_clusters):
-            self.cluster_centers_[i] = X[i]
+        self.cluster_centers_ = np.array([X[i] for i in range(self.n_clusters)])
         # !- Optimization process
-        for _ in range(self.max_iter):
+        for max_iter in range(self.max_iter):
             self.classes_ = {}
             for i in range(self.n_clusters):
                 self.classes_[i] = []
@@ -32,15 +31,14 @@ class KMeans(object):
                 label = self.predict(x)
                 self.classes_[label].append(x)
             # print(self.classes_)
-            prev_cluster_centers = dict(self.cluster_centers_)
+            prev_cluster_centers = np.array(self.cluster_centers_)
             # !- Compute the mean of the classified data
-            for label in self.classes_:
-                self.cluster_centers_[label] = np.mean(self.classes_[label], axis=0)
+            self.cluster_centers_ = np.array([np.mean(self.classes_[label], axis=0) for label in self.classes_])
             # !- Optimization
             optimized = False
-            for label in self.cluster_centers_:
-                old = prev_cluster_centers[label]
-                new = self.cluster_centers_[label]
+            for _ in self.cluster_centers_:
+                old = prev_cluster_centers
+                new = self.cluster_centers_
                 percent_change = (new - old) / old * 100
                 if np.sum(percent_change) < self.tol:
                     optimized = True
@@ -49,13 +47,12 @@ class KMeans(object):
                 break
 
     def predict(self, X):
-        dists = [self.__distance(self.cluster_centers_[centroid], X) for centroid in self.cluster_centers_]
+        dists = [self.__distance(centroid, X) for centroid in self.cluster_centers_]
         return dists.index(min(dists))
 
     @classmethod
     def __distance(cls, a, b):
-        # return euclidean(a, b)
-        return np.linalg.norm(a - b)
+        return euclidean(a, b)
 
     @staticmethod
     def visualize(X, centroids=None, labels=None):
@@ -65,20 +62,20 @@ class KMeans(object):
                 plt.scatter(x[0], x[1], s=50, c=colors[labels[i]])
         else:
             plt.scatter(X[:, 0], X[:, 1], s=50)
-        if centroids:
-            for centroid in centroids:
-                centroid = centroids[centroid]
-                plt.scatter(centroid[0], centroid[1], s=70, c='k', marker='x', linewidth=3)
+        if centroids is not None:
+            plt.scatter(centroids[:, 0], centroids[:, 1], s=70, c='k', marker='x', linewidth=3)
         plt.show()
 
 
 if __name__ == '__main__':
-    X = np.array([
-        [1, 2], [8, 7], [3, 9], [2, 6], [4, 1], [7, 9],
-        [8, 8], [7, 7], [1, 7], [1, 8], [4, 2], [2.5, 3]
-    ])
-    # X = np.array([[1, 2], [3, 2], [2, 3], [3, 1], [6, 8], [8, 6], [7, 7], [8, 9], [9, 7], [8, 8]])
-    clf = KMeans(n_clusters=3)
+    # X = np.array([
+    #     [1, 2], [8, 7], [3, 9], [2, 6], [4, 1], [7, 9],
+    #     [8, 8], [7, 7], [1, 7], [1, 8], [4, 2], [2.5, 3]
+    # ])
+    X = np.array([[1, 2], [3, 2], [2, 3], [3, 1], [6, 8], [8, 6], [7, 7], [8, 9], [9, 7], [8, 8]])
+    clf = KMeans(n_clusters=2)
     clf.fit(X)
     labels = [clf.predict(x) for x in X]
+    print('Labels', labels)
+    print('Centroids', clf.cluster_centers_)
     clf.visualize(X, clf.cluster_centers_, labels)
