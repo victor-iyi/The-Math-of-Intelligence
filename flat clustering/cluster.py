35acba07142d617ee40492bea5cf46447f2f81f6
victor-iyiola <javafolabi@gmail.com>
2017-09-06 21:29:21 +0100

    Implementing the KMeans Algorithm

--- flat clustering/cluster.py
+++ flat clustering/cluster.py
@@ -7,19 +7,53 @@
 """
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.spatial.distance import euclidean
 
 
 class KMeans(object):
     def __init__(self, n_clusters=8, max_iter=300, tol=1e3):
+        self.cluster_centers_ = dict()
+        self.classes_ = None
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.tol = tol
 
     def fit(self, X):
-        pass
+        for i in range(self.n_clusters):
+            self.cluster_centers_[i] = X[i]
+        # !- Optimization process
+        for _ in range(self.max_iter):
+            self.classes_ = {}
+            for i in range(self.n_clusters):
+                self.classes_[i] = []
+            # !- Loop through all features and calculate distance
+            for i, x in enumerate(X):
+                dists = [self.__distance(centroid, x) for centroid in self.cluster_centers_]
+                label = dists.index(min(dists))
+                self.classes_[label].append(x)
+            prev_cluster_centers = dict(self.cluster_centers_)
+            # !- Compute the mean of the classified data
+            for label in self.classes_:
+                self.cluster_centers_[label] = np.mean(self.classes_[label], axis=0)
+            # !- Optimization
+            optimized = False
+            for label in self.cluster_centers_:
+                old = prev_cluster_centers[label]
+                new = self.cluster_centers_[label]
+                percent_change = (new - old) / old * 100
+                if np.sum(percent_change) < self.tol:
+                    optimized = True
+                    break
+            if optimized:
+                break
 
     def predict(self, X):
-        pass
+        dists = [self.__distance(centroid, X) for centroid in self.cluster_centers_]
+        return dists.index(min(dists))
+
+    @classmethod
+    def __distance(cls, a, b):
+        return euclidean(a, b)
 
     @staticmethod
     def visualize(X, centroids, labels):
