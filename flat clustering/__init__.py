3ea7b7ede3417987308a56974596998c440da3ec
victor-iyiola <javafolabi@gmail.com>
2017-09-06 14:24:02 +0100

    Constructing the body

--- flat clustering/__init__.py
+++ flat clustering/__init__.py
@@ -8,4 +8,30 @@
 
 import pandas as pd
 import numpy as np
+import warnings
 
+warnings.filterwarnings('ignore')
+
+
+def process_data(df):
+    """
+    Processes a dataframe in order to handle for non-numeric data
+    :type df: pd.DataFrame
+    :param df: the dataframe containing the data
+    """
+    columns = df.columns.values
+    for col in columns:
+        pass
+
+
+def main():
+    df = pd.read_excel("../datasets/titanic.xls")
+    df.drop(['body', 'name'], 1, inplace=True)
+    df.convert_objects(convert_numeric=True)
+    df.fillna(0, inplace=True)
+    # print(df.head())
+    process_data(df)
+
+
+if __name__ == '__main__':
+    main()
