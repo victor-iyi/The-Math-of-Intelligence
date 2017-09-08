9a0459546f0c1a0946d6f2c9db72502f31970581
victor-iyiola <javafolabi@gmail.com>
2017-09-09 00:32:19 +0100

    Computing the error

--- neural_network/mlp.py
+++ neural_network/mlp.py
@@ -11,6 +11,7 @@ import numpy as np
 
 class MultiLayerPerceptron(object):
     def __init__(self, layers=None, learning_rate=1e-3):
+        self.logits = []
         self.hidden_layers = dict()
         self.output_layer = dict()
         self.activation = []
@@ -26,28 +27,25 @@ class MultiLayerPerceptron(object):
             self.hidden_layers[i]['weight'] = 2 * np.random.random([prev, layer]) - 1
         self.output_layer['weight'] = 2 * np.random.random([self.layers[-1], len(y[0])]) - 1
         # !- Perform forward propagation
-        y_hat = self.forward_prop(X)
-        print(y_hat)
+        for _ in range(n_iter)[:5]:
+            self.backwardProp(X, y)
 
-    def forward_prop(self, X):
-
+    def forwardProp(self, X):
         self.activation = []
+        self.logits = []
         for n in range(self.n_layers):
             x = self.activation[-1] if n > 0 else X
-            act = self.__sigmoid(np.dot(x, self.hidden_layers[n]['weight']))
+            logit = np.dot(x, self.hidden_layers[n]['weight'])
+            act = self.__sigmoid(logit)
+            self.logits.append(logit)
             self.activation.append(act)
         y_hat = self.__sigmoid(np.dot(self.activation[-1], self.output_layer['weight']))
         return y_hat
 
-    def backward_prop(self, X, y):
-        y_hat = self.forward_prop(X)
-        error = (y - y_hat) ** 2
-        for i in range(self.n_layers):
-            delta = np.multiply(error, self.__sigmoid(self.activation[i]))
-            gradient = np.dot(delta, self.hidden_layers[i]['weight'].T)
-            # !- Weight update
-            self.hidden_layers[i]['weight'] -= (gradient * self.learning_rate)
-            error = np.multiply(delta, self.__sigmoid(self.activation[i+1]))  # error for next layer
+    def backwardProp(self, X, y):
+        yHat = self.forwardProp(X)
+        error = (y - yHat) ** 2
+        print(error)
 
     @staticmethod
     def __sigmoid(X, derivative=False):
@@ -58,5 +56,5 @@ if __name__ == '__main__':
     X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 0], [1, 0, 1]])
     y = np.array([[0], [0], [1], [1]])
 
-    clf = MultiLayerPerceptron([5, 4, 3])
+    clf = MultiLayerPerceptron([5, 4, 3, 3])
     clf.fit(X, y)
