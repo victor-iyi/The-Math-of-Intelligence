0c96f0659122f56fe4e83b9360e5d6f6fe13d20a
victor-iyiola <javafolabi@gmail.com>
2017-09-11 05:19:47 +0100

    Convolutional Neural Network

---
+++ convolutional neural network/__init__.py
@@ -0,0 +1,7 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 11 September, 2017 @ 5:17 AM.
+  Copyright © 2017. Victor. All rights reserved.
+"""
---
+++ convolutional neural network/cnn.py
@@ -0,0 +1,25 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 11 September, 2017 @ 5:17 AM.
+  Copyright © 2017. Victor. All rights reserved.
+"""
+
+
+class CNN(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, X, y):
+        pass
+
+
+if __name__ == '__main__':
+    pass
