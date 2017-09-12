2978f857923d9a2282e47f97d1189a3fab932c2c
victor-iyiola <javafolabi@gmail.com>
2017-09-13 00:24:47 +0100

    Making a LinearRegression from scratch

---
+++ linear regression/linear_model.py
@@ -0,0 +1,7 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 13 September, 2017 @ 12:24 AM.
+  Copyright (c) 2017. Victor. All rights reserved.
+"""
