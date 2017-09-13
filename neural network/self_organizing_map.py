03aabc3deeb0cb43117f1d8d34203b273371e421
victor-iyiola <javafolabi@gmail.com>
2017-09-13 20:05:02 +0100

    Self organizing map

---
+++ neural network/self_organizing_map.py
@@ -0,0 +1,7 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  Created on 13 September, 2017 @ 8:04 PM.
+  Copyright © 2017. Victor. All rights reserved.
+"""
