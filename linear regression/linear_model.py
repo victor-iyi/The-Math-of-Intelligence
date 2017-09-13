9b97cee79ca4544100edabb43d1343ad736c7132
victor-iyiola <javafolabi@gmail.com>
2017-09-13 01:28:29 +0100

    Predicting with LinearRegression although not satisfied with not multiplying x

--- linear regression/linear_model.py
+++ linear regression/linear_model.py
@@ -5,3 +5,64 @@
   Created on 13 September, 2017 @ 12:24 AM.
   Copyright (c) 2017. Victor. All rights reserved.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class LinearRegression:
+    def __init__(self, learning_rate=1e-4):
+        self.learning_rate = learning_rate
+        self.W = np.array([])
+        self.b = np.array([])
+
+    def fit(self, X, y, num_iter=10000):
+        weight_shape = [X.shape[1], y.shape[1]]
+        bias_shape = [y.shape[1]]
+        self.W = np.random.random(weight_shape)
+        self.b = np.random.random(bias_shape)
+        for _ in range(num_iter):
+            self.__gradientDescent(X, y)
+
+    def predict(self, x):
+        return np.dot(x, self.W) + self.b
+
+    def __cost(self, X, y):
+        yHat = self.predict(X)
+        J = 0.5 * np.sum((yHat - y) ** 2)
+        return J
+
+    def __gradientDescent(self, X, y):
+        m = len(X)
+        w_gradient = 0
+        b_gradient = 0
+        for i, _ in enumerate(X):
+            x = X[i]
+            y_ = y[i]
+            w_gradient += (self.predict(x) - y_) / m
+            b_gradient += (self.predict(x) - y_) / m
+        self.W = self.W - (self.learning_rate * w_gradient)
+        self.b = self.b - (self.learning_rate * b_gradient)
+
+
+if __name__ == '__main__':
+    X = np.array([
+        [2104, 5, 1, 45],
+        [1416, 3, 2, 40],
+        [1534, 3, 2, 30],
+        [852, 2, 1, 36],
+    ])
+    y = np.array([[466], [232], [315], [172]])
+    X_pred = np.array([
+        [700, 1, 1, 30],
+        [1200, 3, 2, 35],
+        [2500, 7, 3, 42],
+    ])
+    # !- Using the model
+    clf = LinearRegression()
+    clf.fit(X, y)
+    print(clf.predict(X_pred))
+    # !- Visualizing the model
+    plt.scatter(X[:, 0], y)
+    plt.plot(X_pred[:, 0], clf.predict(X_pred))
+    plt.show()
