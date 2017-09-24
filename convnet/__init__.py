0c1e1e2ca6eb656379ff7483202a3c65e73c8d92
victor-iyiola <javafolabi@gmail.com>
2017-09-24 23:45:48 +0100

    Convolutional Neural Network package

--- convnet/__init__.py
+++ convnet/__init__.py
@@ -5,3 +5,14 @@
   Created on 11 September, 2017 @ 5:17 AM.
   Copyright © 2017. Victor. All rights reserved.
 """
+
+from .cnn import CNN
+
+"""
+The :mod:`convnet` module implements generalized image classification specific
+models. It includes CNN
+"""
+
+__all__ = [
+    'CNN',
+]
